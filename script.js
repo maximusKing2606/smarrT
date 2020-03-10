4234e10687d48c1cee7ec657d696228b888d5c1f
maximusKing2606 <hripamaksa@gmail.com>
2020-03-10 13:38:15 +0200

    Some chenges

--- script.js
+++ script.js
@@ -2,7 +2,49 @@
    
    $(function(){
 
-  
+    let header = $(".header"),
+
+        introH = $(".intro").innerHeight(),
+
+        scrollOffset = $(window).scrollTop();
+
+
+    // Fixed Header
+    checkScroll(scrollOffset);
+
+    $(window).on("scroll", function(){
+
+     scrollOffset = $(this).scrollTop();
+
+     checkScroll(scrollOffset);
+});
+
+function checkScroll(scrollOffset) {
+if (scrollOffset >= introH) {
+    header.addClass("fixed");
+} else{
+    header.removeClass("fixed");
+}
+}
+
+// Scroll
+
+$("[data-scroll]").on("click", function(event){
+    event.preventDefault();
+
+    let $this = $(this),
+     blockId = $this.data('scroll'),
+    blockOffset = $(blockId).offset().top;
+
+    $("#nav a").removeClass("active");
+    $this.addClass("active");
+
+    $("html,body").animate({
+        scrollTop: blockOffset
+    },1000)
+});
+
+// Slider
    
    $("[data-slider]").slick({
     infinite: true,
@@ -11,6 +53,20 @@
     slidesToScroll: 1
 });
 
+$(".nav-toggle").on("click", function(event){
+    event.preventDefault();
 
+   
+    $(".nav__link").addClass("active");
+   
+
+});
+
+$(".nav__link").on("click", function(){
+
+
+    $(".nav__link").removeClass("active");
+
+})
 
 });
